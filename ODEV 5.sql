7c27f29e7fb74264089177a5b73b559f757dc8fb
Sefa Yılmaz <113579147+sefayilmaz14@users.noreply.github.com>
2024-08-22 01:14:49 +0300

    Add files via upload

---
+++ ODEV 5.sql
@@ -0,0 +1,15 @@
+--SELECT * FROM Film
+--WHERE title LIKE '%n'
+--ORDER BY length DESC
+--LIMIT 5;
+
+--SELECT * FROM Film
+--WHERE title LIKE '%n'
+--ORDER BY length ASC
+--OFFSET 5
+--LIMIT 5;
+
+SELECT * FROM customer
+WHERE store_id = 1
+ORDER BY last_name ASC
+LIMIT 4;
